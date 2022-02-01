65ef24d1b1dbfbf20f0e212a79d4db8ea6a3a11b
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2022-02-01 18:51:53 +0600

    wassup

---
+++ math.js
@@ -0,0 +1,8 @@
+let num = Math.ceil(40.3432);
+console.log(num);
+let num1 = Math.floor(40.3432);
+console.log(num1);
+let num2 = Math.round(40.5432);
+console.log(num2);
+let num3 = Math.ceil(Math.random() * 6);
+console.log(num3);
